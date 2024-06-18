f915652e0579933560e39b2974342a0a2a902c51
QiJiang <77928612+QiJiang-QJ@users.noreply.github.com>
2024-06-18 21:24:31 +0800

    Add files via upload

---
+++ run_fate.py
@@ -0,0 +1,9 @@
+from config_Veres import config, init_config
+import src.train as train
+from src.evaluation import evaluate_fit
+
+
+args = config()
+config = train.run(args, init_config)
+evaluate_fit(config, init_config, use_loss='emd')
+

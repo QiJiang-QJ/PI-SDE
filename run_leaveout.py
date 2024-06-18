927563ad725550ed32ce846ba49547964f9f662e
QiJiang <77928612+QiJiang-QJ@users.noreply.github.com>
2024-06-18 21:25:14 +0800

    Add files via upload

---
+++ run_leaveout.py
@@ -0,0 +1,17 @@
+from config_Veres import config, init_config
+import src.train as train
+from src.evaluation import evaluate_fit_leaveout
+
+args = config()
+
+args.task = 'leaveout'
+args.data_path = 'data/Veres/leaveout7/fate_train.pt'
+
+
+config = train.run_leaveout(args, init_config, leaveouts=[7])
+evaluate_fit_leaveout(config, init_config, leaveouts=[7], use_loss='emd')
+
+
+# config = train.run_leaveout(args, init_config,leaveouts=[3,4])
+# evaluate_fit_leaveout(config, init_config,leaveouts=[3,4],use_loss='emd')
+ 
